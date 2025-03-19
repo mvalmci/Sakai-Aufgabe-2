82f75be08cf1d59f2348ac49da63887be2652709
mvalmci <m.valenta@mci4me.at>
2025-03-19 21:25:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
     experiment = build_experiment(
         experiment_name=input("Bitte geben Sie dem Experiment einen Namen: "),
         date=datetime.today().strftime("%Y-%m-%d"),
-        supervisor=input("Bitte geben Sie den Namen des Versuchsleiters ein: "),
+        supervisor=input("Bitte geben Sie den Namen des Versuchsleiters bzw. der Versuchsleiterin ein: "),
         subject="max_hr_test"
     )
-    age_general = int(input("Bitte geben Sie das Alter des Patienten ein: "))
-    sex_general = input("Bitte geben Sie das Geschlecht des Patienten ein! (male/female): ")
+    age_general = int(input("Bitte geben Sie das Alter des Patienten bzw. der Patientin ein: "))
+    sex_general = input("Bitte geben Sie das Geschlecht des Patienten bzw. der Patientin ein! (male/female): ")
     patient = build_person(
-        first_name=input("Wie lautet der Vorname des Patienten?: "),
-        last_name=input("Wie lautet der Nachname des Patienten?: "),
+        first_name=input("Wie lautet der Vorname des Patienten bzw. der Patientin?: "),
+        last_name=input("Wie lautet der Nachname des Patienten bzw. der Patientin?: "),
         sex=sex_general,
         age=age_general
     )
